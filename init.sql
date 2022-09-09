21dc47d1a8a101185f64b532ce2ade9a0f3ec893
N0V@ <106326095+refunringle@users.noreply.github.com>
2022-09-09 22:02:58 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,40 +1,42 @@
--- DROP TABLE IF EXISTS user CASCADE;
--- DROP TABLE IF EXISTS Post CASCADE;
--- DROP TABLE IF EXISTS Post_Comment CASCADE;
--- DROP TABLE IF EXISTS guest_user CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS Post CASCADE;
+DROP TABLE IF EXISTS Post_Comment CASCADE;
+DROP TABLE IF EXISTS guest_user CASCADE;
 
-CREATE TABLE user(
+    -- MY DATA BASE NAME IS blog **
+
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL
+	name VARCHAR (50) UNIQUE NOT NULL,
+	email VARCHAR (255) UNIQUE NOT NULL,
+	password VARCHAR (50) NOT NULL
 	-- created_on TIMESTAMP NOT NULL,
     -- last_login TIMESTAMP
 );
 
-CREATE TABLE post(
+CREATE TABLE post (
     id SERIAL PRIMARY KEY,
-    user_id INT FOREIGN KEY references user(id),
+    user_id INT references users(id),
     title VARCHAR(150),
     category VARCHAR(100),
     -- summary TEXT,
     content TEXT, 
-    publish_time TIMESTAMP NOT NULL VALUES(default now()),
-    -- tag SERIAL 
+    publish_time TIMESTAMP NOT NULL
+    -- tag SERIAL  
 );
 
-CREATE TABLE Post_Comment(
-    id SERIAL,
-    post_id INT FOREIGN KEY references post(id)),
+CREATE TABLE Post_Comment (
+    id SERIAL PRIMARY KEY,
+    post_id INT references post(id),
     Guest_name VARCHAR(100),
-    publish_Time TIMESTAMP NOT NULL VALUES(default now()),
+    publish_Time TIMESTAMP NOT NULL ,
     comment TEXT
 );
 
 CREATE TABLE guest_user(
     id SERIAL PRIMARY KEY,
-    post_id INT FOREIGN KEY references Post_Comment(id),
+    post_id INT references Post_Comment(id),
     Guest_name VARCHAR(100),
-    GUEST_Time TIMESTAMP NOT NULL VALUES(default now()),
+    GUEST_Time TIMESTAMP NOT NULL ,
     comment TEXT
 );
